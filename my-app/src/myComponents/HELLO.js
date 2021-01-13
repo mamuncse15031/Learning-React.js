582c13955ed462ff7c06bae9d6810e0737209944
Md. Abdullah Al Mamun <46086604+mamuncse15031@users.noreply.github.com>
2021-01-13 08:19:47 +0600

    Function button click parameter

--- my-app/src/myComponents/HELLO.js
+++ my-app/src/myComponents/HELLO.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 function hello(props){
 
-    function doThis(){
+    function doThis(a){
 
-        alert("Button is Clicked");
+        alert(a);
     }
 return(
     <div>
-        <button onClick={doThis}>Click me</button>
+        <button onClick={doThis.bind(this,"I am from function")}>Click me</button>
         <h1> Name: {props.name} and Age: {props.age} </h1>
     </div>
     
